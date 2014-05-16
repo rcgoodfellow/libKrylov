8ea7d44044432a47a6c4740976e24a0a2a44743a
Ryan Goodfellow <rgoodfel@isi.edu>
2014-05-16 10:21:38 -0700

    initial

---
+++ Vec.hxx
@@ -0,0 +1,54 @@
+#ifndef _KRYLOV_VEC_
+#define _KRYLOV_VEC_
+
+#include <cstddef>
+#include <type_traits>
+#include <initializer_list>
+#include <cassert>
+#include <iterator>
+#include <cmath>
+#include <mm_malloc.h>
+
+namespace kry
+{
+
+template<size_t N>
+class Vec
+{
+  public:
+    Vec(std::initializer_list<double> d)
+    {
+      assert(d.size() == N);
+      for(auto it = d.begin(); it != d.end(); ++it) 
+        data[std::distance(it, d.begin())] = *it;
+    }
+    Vec() { }
+    double *data = (double*)_mm_malloc(sizeof(double)*N, 64);
+    static constexpr size_t size{N};
+    double & operator[](size_t i){ return data[i]; }
+    double const & operator[](size_t i) const { return data[i]; }
+
+};
+
+template<size_t N>
+double p_norm(const Vec<N> &v)
+{
+  double *squares = (double*)_mm_malloc(sizeof(double)*v.size, 64);
+  squares[0:v.size] = pow(v.data[0:v.size], 2);
+
+  double sum{0};
+  for(size_t i=0; i<v.size; ++i) sum += squares[i];
+  return sqrt(sum);
+}
+
+template<size_t N>
+double s_norm(const Vec<N> &v)
+{
+  double sum{0};
+  for(size_t i=0; i<v.size; ++i) sum += pow(v[i], 2);
+  return sqrt(sum);
+}
+
+}
+
+#endif
---
+++ VecTests.cxx
@@ -0,0 +1,41 @@
+#include "Vec.hxx"
+#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+
+int main()
+{
+  std::cout << "Vector Tests" << std::endl;
+  
+
+  kry::Vec<10000000> v{}; 
+  v.data[0:10000000] = 1.4747;
+
+  auto s = high_resolution_clock::now();
+  double vn = p_norm(v);
+  auto f = high_resolution_clock::now();
+  auto d = f - s;
+  std::cout 
+    << "[SIMD]" 
+    << duration_cast<microseconds>(d).count() << "us"
+    << std::endl;
+  std::cout 
+    << "VNorm SIMD: " << vn 
+    << std::endl;
+  
+  s = high_resolution_clock::now();
+  vn = s_norm(v);
+  f = high_resolution_clock::now();
+  d = f - s;
+  std::cout 
+    << "[PSEQ]" 
+    << duration_cast<microseconds>(d).count() << "us"
+    << std::endl;
+  std::cout 
+    << "VNorm PSEQ: " << vn 
+    << std::endl;
+
+
+  return 0;
+}
